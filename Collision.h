8a10fce43e7b24980af1efccc8a825f6beabf7fc
Sidar Talei <sidar_talei@hotmail.com>
2012-06-18 19:29:24 +0200

    inline overlap functions

--- Collision.h
+++ Collision.h
@@ -1,14 +1,23 @@
 #ifndef _COLLISION_H_
 #define _COLLISION_H_
 
-#include 
+#include <SFML/Graphics.hpp>
 namespace st{
 	namespace management{
-inline bool checkSpriteOverLap(){
-	
-}
-
-};
+		namespace collision{
+			//=============================================================================
+			inline bool spriteOverlaps(const sf::Sprite &sp, const sf::Sprite &otherSp){
+					return sp.getGlobalBounds().intersects(otherSp.getGlobalBounds());
+			}
+			//=============================================================================
+			
+			//=============================================================================
+			inline bool rectOverlaps(const sf::Rect<float> rect, const sf::Rect<float> otherRect){
+					return rect.intersects(otherRect);
+			}
+			//=============================================================================
+		};
+	};
 };
 
 #endif 
